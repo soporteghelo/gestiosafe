cbdce4eeff9a144ffb32e7eb135c37d11a90c2be
soporteghelo <soporte.ghelo@gmail.com>
2026-02-03 17:02:04 -0500

    chore: actualizar URL de Google Apps Script con nueva implementacion (fix de seguridad)

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@
 
 // URL de Google Apps Script - CAMBIA SOLO AQUÍ
 // Después de hacer una nueva implementación en Apps Script, actualiza esta URL
-export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwoUVrRuKaPzCq53iwh5y9wVennUXAoXM6Rwtkxp2pDuYN1m_46XXq7Tu_bkLzL6reZtA/exec";
+export const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbx22oVUjJJPtROPXIBfeyr4MGhTofH__Qg_woI4DGgSJgwxHrR0zupJgxw4yiIq4r8pDA/exec";
 
 // Mercado Pago Public Key
 export const MERCADOPAGO_PUBLIC_KEY = import.meta.env.VITE_MERCADOPAGO_PUBLIC_KEY || "APP_USR-NOT_CONFIGURED";
